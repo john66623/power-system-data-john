944f839ce7e1ad3d22f8f75cf575a681856d8cce
john66623 <john.h.bo@lyse.net>
2026-09-22 10:22:34 +0200

    La til plots for døgnprofil og modell

--- oving5/oving5_kode.py
+++ oving5/oving5_kode.py
@@ -22,6 +22,7 @@ t_highres = np.linspace(0, 23, 500) # Lager tilsvarende array med høyere opplø
 
 # Definerer variabler til modellen
 grunnlast = dognprofil["Consumption"].min()
+
 morgenprofil = dognprofil.loc[
     "2019-01-15 07:00" : "2019-01-15 10:00", 
     "Consumption"]
@@ -34,11 +35,11 @@ kveldsprofil = dognprofil.loc[
     "Consumption"]
 kveld_amp = kveldsprofil.max() - grunnlast
 kveld_tid = kveldsprofil.idxmax().hour
-kveld_bredde = 3.5
+kveld_bredde = 4
 
 natt_amp = kveld_amp
 natt_tid = kveld_tid - 24
-natt_bredde = 5
+natt_bredde = kveld_bredde
 
 # Definerer modellen med ligninger
 morgen_peak = morgen_amp * np.exp(-(((t_highres - morgen_tid)**2) / (2 * morgen_bredde**2)))
@@ -46,18 +47,44 @@ kveld_peak = kveld_amp * np.exp(-(((t_highres - kveld_tid)**2) / (2 * kveld_bred
 natt_peak = natt_amp * np.exp(-(((t_highres - natt_tid)**2) / (2 * natt_bredde**2)))
 modell = grunnlast + morgen_peak + kveld_peak + natt_peak
 
-print(morgen_peak.dtype)
+# Skriver ut parameterverdier
+print("Amplitude for morgenen er ", morgen_peak, "Ved time ", morgen_tid, "og bredde ", morgen_bredde)
+print("Amplitude for kvelden er ", kveld_peak, "Ved time ", kveld_tid, "og bredde ", kveld_bredde)
+print("Amplitude for natten er ", natt_peak, "Ved time ", natt_tid, "og bredde ", natt_bredde)
 
-# Plotter modellen sammen med døgnprofilen
+
+# Plotter døgnprofilen med observerte data
 plt.plot(t, dognprofil["Consumption"], label = "Døgnprofil")
+plt.grid(True)
+plt.legend()
+plt.xlabel("Time i døgnet")
+plt.ylabel("Forbruk i MW")
+plt.title("Observerte data for forbruk 15. januar 2019")
+
+# Plotter modellen
+plt.figure()
 plt.plot(t_highres, modell, label = "Modellen")
-plt.plot(t_highres, morgen_peak + grunnlast, linestyle = "--", linewidth = 0.7, label = "Morgen peak")
-plt.plot(t_highres, kveld_peak + grunnlast, linestyle = "--", linewidth = 0.7, label = "Kveld peak")
-plt.plot(t_highres, natt_peak + grunnlast, linestyle = "--", linewidth = 0.7, label = "Natt peak")
+plt.plot(t_highres, morgen_peak + grunnlast, linestyle = "--", linewidth = 0.7, label = f"Morgen, A = {morgen_amp:.1f}, tid = {morgen_tid:.1f}, bredde = {morgen_bredde:.1f}")
+plt.plot(t_highres, kveld_peak + grunnlast, linestyle = "--", linewidth = 0.7, label = f"Kveld, A = {kveld_amp:.1f}, tid = {kveld_tid:.1f}, bredde = {kveld_bredde:.1f}")
+plt.plot(t_highres, natt_peak + grunnlast, linestyle = "--", linewidth = 0.7, label = f"Natt, A = {natt_amp:.1f}, tid = {natt_tid:.1f}, bredde = {natt_bredde:.1f}")
 plt.legend()
 plt.ylim(dognprofil["Consumption"].min() - 1000, dognprofil["Consumption"].max() + 1000)
 plt.xlabel("Time i døgnet")
 plt.ylabel("Forbruk i MW")
 plt.title("Modell for forbruk 15. januar 2019")
+plt.grid(True)
+
+# Plotter modellen sammen med døgnprofilen
+plt.figure()
+plt.plot(t, dognprofil["Consumption"], label = "Døgnprofil")
+plt.plot(t_highres, modell, label = "Modellen")
+plt.plot(t_highres, morgen_peak + grunnlast, linestyle = "--", linewidth = 0.7, label = f"Morgen, A = {morgen_amp:.1f}, tid = {morgen_tid:.1f}, bredde = {morgen_bredde:.1f}")
+plt.plot(t_highres, kveld_peak + grunnlast, linestyle = "--", linewidth = 0.7, label = f"Kveld, A = {kveld_amp:.1f}, tid = {kveld_tid:.1f}, bredde = {kveld_bredde:.1f}")
+plt.plot(t_highres, natt_peak + grunnlast, linestyle = "--", linewidth = 0.7, label = f"Natt, A = {natt_amp:.1f}, tid = {natt_tid:.1f}, bredde = {natt_bredde:.1f}")
+plt.legend()
+plt.ylim(dognprofil["Consumption"].min() - 1000, dognprofil["Consumption"].max() + 1000)
+plt.xlabel("Time i døgnet")
+plt.ylabel("Forbruk i MW")
+plt.title("Modell og observerte data for forbruk 15. januar 2019")
 plt.grid(True)
 plt.show()
