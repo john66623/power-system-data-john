27a4f02c22e6d01b87f153482b7cce1c3ade702e
john66623 <john.h.bo@lyse.net>
2026-09-24 15:16:30 +0200

    La til kode for plotting av soldata fra PVgis

--- oving6/oving6_kode.py
+++ oving6/oving6_kode.py
@@ -4,6 +4,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Leser inn soldata, gjør om til datetime index
+soldata = pd.read_csv("oving6/soldata_2023.csv", skiprows=8, nrows=8760)
+
+soldata["time"] = pd.to_datetime(
+    soldata["time"], 
+    format = "%Y%m%d:%H%M").dt.round("h")
+
+soldata = soldata.set_index("time")
+
+print(soldata.head())
+
+print(soldata["G(i)"].idxmax())
+
 t = np.linspace(0, 23, 500) # Lager tidsakse med 24 timer
 
 # Setter verdier for innstråling
@@ -14,10 +27,19 @@ tid_straling = 13
 # Modellerer solstråling
 modell_straling = maks_straling * np.exp(-(((t - tid_straling)**2) / (2 * bredde_straling**2)))
 
-# Plotter
+# Plotter modellen
 plt.plot(t, modell_straling, label = "Stråling i W/m^2")
 plt.xlabel("Tid i timer")
 plt.ylabel("Stråling")
 plt.title("Modell for solstråling over et døgn")
 plt.grid(True)
+plt.legend()
+
+# Plotter PVgis data alene
+plt.figure()
+plt.plot(soldata.loc["2023-05-23", "G(i)"], label = "Solstråling i W/m^2")
+plt.grid(True)
+plt.xlabel("Tid i timer")
+plt.ylabel("Stråling")
+plt.legend()
 plt.show()
