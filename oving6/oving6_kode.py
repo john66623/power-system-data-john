e99b60a1fe6f8a4885002afe4d8dfa4241e2453b
john66623 <john.h.bo@lyse.net>
2026-09-24 14:29:27 +0200

    Opprettet oving6 mappe, opprettet oving6 kode, kodet modell

---
+++ oving6/oving6_kode.py
@@ -0,0 +1,23 @@
+# Kode for øving 6
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+t = np.linspace(0, 23, 24) # Lager tidsakse med 24 timer
+
+# Setter verdier for innstråling
+maks_straling = 800
+bredde_straling = 3
+tid_straling = 13
+
+# Modellerer solstråling
+modell_straling = maks_straling * np.exp(-(((t - tid_straling)**2) / (2 * bredde_straling**2)))
+
+# Plotter
+plt.plot(t, modell_straling, label = "Stråling i W/m^2")
+plt.xlabel("Tid i timer")
+plt.ylabel("Stråling")
+plt.title("Modell for solstråling over et døgn")
+plt.grid(True)
+plt.show()
